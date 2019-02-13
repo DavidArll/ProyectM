ac91afdc35066720a271acef1c999e3427994efc
David Arellano <alx.arellanogmail.com>
2019-02-13 17:15:24 -0600

    Homepage design

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './css/creative.css'
 class Navigation extends Component {
     state = {  }
     render() { 
         return ( 
-            <nav id="navbar-main" className="navbar navbar-inverse">
-                <div className="container">
-                    <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">      
-                    </button>
-                    <a className="navbar-brand topnav" href="#">Hbr Web Design</a>
-                    </div>
-                    <div id="navbar" className="collapse navbar-collapse">
-                        <ul className="nav navbar-nav">
-                            <li><a href="#about">ABOUT ME</a></li>
-                            <li><a href="#portfolio">PORTFOLIO</a></li>
-                            <li><a href="#contact">CONTACT</a></li>
-                        </ul>
+            <React.Fragment>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Dorzav</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Sobre nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Eventos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+        <header class="masthead">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center justify-content-center text-center">
+                            <div class="col-lg-10 align-self-end">
+                                <h1 class="text-uppercase text-white font-weight-bold">DORZAV Desing</h1>
+                                <hr class="divider my-4" />
+                            </div>
+                            <div class="col-lg-8 align-self-baseline">
+                                <p class="text-white-75 font-weight-light mb-5">Recamaras y mas..</p>
+                                {/* <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a> */}
+                            </div>
                     </div>
                 </div>
-            </nav>
+        </header>
+        </React.Fragment>
          );
     }
 }
